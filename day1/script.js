66f2fd42a27e963a64372eca586695a92ad4a268
Akash-nath29 <anath5440@gmail.com>
2023-12-26 23:37:05 +0530

    Started #100DaysOfCode challange

---
+++ day1/script.js
@@ -0,0 +1,65 @@
+const RANDOM_QUOTE_API_URL = 'https://cors-anywhere.herokuapp.com/http://api.quotable.io/random';
+const quoteDisplayElement = document.getElementById('quoteDisplay');
+const quoteInputElement = document.getElementById('quoteInput');
+const timerElement = document.getElementById('timer');
+
+quoteInputElement.addEventListener('input', () => {
+  const arrayQuote = quoteDisplayElement.querySelectorAll('span');
+  const arrayValue = quoteInputElement.value.split('');
+
+  let correct = true;
+  arrayQuote.forEach((characterSpan, index) => {
+    const character = arrayValue[index];
+    if (character == null) {
+      characterSpan.classList.remove('correct');
+      characterSpan.classList.remove('incorrect');
+      correct = false;
+    } else if (character === characterSpan.innerText) {
+      characterSpan.classList.add('correct');
+      characterSpan.classList.remove('incorrect');
+    } else {
+      characterSpan.classList.remove('correct');
+      characterSpan.classList.add('incorrect');
+      correct = false;
+    }
+  });
+
+  if (correct) renderNewQuote();
+});
+
+async function getRandomQuote() {
+  return await fetch(RANDOM_QUOTE_API_URL)
+    .then(response => response.json())
+    .then(data => data.content)
+    .catch(error => {
+      console.error('Error fetching quote:', error);
+    });
+}
+
+async function renderNewQuote() {
+  const quote = await getRandomQuote();
+  quoteDisplayElement.innerHTML = '';
+  quote.split('').forEach(character => {
+    const characterSpan = document.createElement('span');
+    characterSpan.innerText = character;
+    quoteDisplayElement.appendChild(characterSpan);
+  });
+  quoteInputElement.value = '';
+  startTimer();
+}
+
+let startTime;
+
+function startTimer() {
+  timerElement.innerText = 0;
+  startTime = new Date();
+  setInterval(() => {
+    timerElement.innerText = getTimerTime();
+  }, 1000);
+}
+
+function getTimerTime() {
+  return Math.floor((new Date() - startTime) / 1000);
+}
+
+renderNewQuote();
